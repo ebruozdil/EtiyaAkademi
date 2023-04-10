63ce4993b6a1ced6e4a3facba340304926826cf9
Ebru Özdil <ebruozdil99@gmail.com>
2023-04-10 09:41:24 +0300

    workshop 4-pair3 10.04.23

---
+++ workshops/workshop4/largestNumberFunction.py
@@ -0,0 +1,9 @@
+def largestNumber():
+    numberList = []
+    for i in range(10):
+        number = int(input("Sayı giriniz : "))
+        numberList.append(number)
+    print(numberList)
+    print("Dizideki en büyük sayı : ", max(numberList))
+
+largestNumber();
